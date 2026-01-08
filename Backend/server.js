91bec7e624183837734fcf3d809390dd10184d97
nagaraaju43 <nagaraaju43@gmail.com>
2026-01-08 18:10:02 +0530

    `Deleted all frontend files and scripts`

--- Backend/server.js
+++ Backend/server.js
@@ -32,7 +32,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS // Your App Password
+        pass: process.env.EMAIL_PASSes // Your App Password
     }
 });
 
